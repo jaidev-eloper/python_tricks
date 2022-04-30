df1ab428293d3f233497e56bb0716f0cbf61bf93
jaidev-eloper <71312025+jaidev-eloper@users.noreply.github.com>
2022-04-30 08:23:32 +0100

    Update chapter7.3_dispatchDicts.py

--- chapter7.3_dispatchDicts.py
+++ chapter7.3_dispatchDicts.py
@@ -9,3 +9,7 @@ def dispatch(operator, x, y):
     return my_dict(x, y).get(operator, lambda: None)
     
 print (dispatch('mul', 5, 2))
+
+## NOT SURE ABOUT THIS - It creates temporary dictionary everytime. For code to be fast, 
+## create a dictionary once as a constant and then reference it when function is called.  
+## How can I do that with x, y as parametres in the values
